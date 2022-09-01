edd9826ba2fa57ef7a5d9d28db995560ffe93585
Anil Oeguetveren <anil.oeguetveren@daimler.com>
2022-09-01 13:44:48 +0200

    Docker Container is added

--- src/main/kotlin/com/feelthesteel/band/webpage/WebpageApplication.kt
+++ src/main/kotlin/com/feelthesteel/band/webpage/WebpageApplication.kt
@@ -2,11 +2,10 @@ package com.feelthesteel.band.webpage
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 
 @SpringBootApplication
 class WebpageApplication
 
 fun main(args: Array<String>) {
-	runApplication<WebpageApplication>(*args)
+    runApplication<WebpageApplication>(*args)
 }
