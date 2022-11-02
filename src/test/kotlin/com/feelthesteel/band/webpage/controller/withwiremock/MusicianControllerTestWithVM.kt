bea582caa0b0443ce84f89a489253c6b05810ebd
anilogutveren <35260926+anilogutveren@users.noreply.github.com>
2022-11-02 18:28:36 +0100

    WireMock Test is started but not finished

--- src/test/kotlin/com/feelthesteel/band/webpage/controller/withwiremock/MusicianControllerTestWithVM.kt
+++ src/test/kotlin/com/feelthesteel/band/webpage/controller/withwiremock/MusicianControllerTestWithVM.kt
@@ -1,28 +1,29 @@
 package com.feelthesteel.band.webpage.controller.withwiremock
 
 import com.feelthesteel.band.webpage.service.impl.MusiciansServiceImpl
+import com.github.jenspiegsa.wiremockextension.ManagedWireMockServer.with
 import com.github.tomakehurst.wiremock.WireMockServer
 import com.github.tomakehurst.wiremock.client.WireMock.aResponse
 import com.github.tomakehurst.wiremock.client.WireMock.get
-import com.github.tomakehurst.wiremock.client.WireMock.options
 import com.github.tomakehurst.wiremock.client.WireMock.stubFor
 import com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo
-import com.github.tomakehurst.wiremock.common.ConsoleNotifier
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration
+import com.github.tomakehurst.wiremock.junit5.WireMockExtension
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock
 import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureWireMock(port = 9999)
 @ActiveProfiles("test")
+@ExtendWith(WireMockExtension::class)
 internal class MusicianControllerTestWithVM() {
 
-    val wiremock: WireMockServer = WireMockServer(options("").withPort(9999))
+    val wiremock: WireMockServer = with(WireMockConfiguration.wireMockConfig().dynamicPort())
 
     @Autowired
     private lateinit var musiciansServiceImpl: MusiciansServiceImpl
@@ -44,52 +45,8 @@ internal class MusicianControllerTestWithVM() {
             get(urlEqualTo("/musicians"))
                 .willReturn(aResponse().withHeader("Content-Type", "application/json").withBody("Hello World!"))
         )
+
         // We're asserting if WireMock responded properly
         assertThat(this.musiciansServiceImpl.getAllMusicians()).isEqualTo(listOf(null))
     }
-
-    /*    private val logger: Logger = LoggerFactory.getLogger(javaClass)
-        private val CONTENT_TYPE = "application/json"
-        private val TRACKING_ID = "abcde"
-
-        @MockBean
-        private lateinit var musiciansServiceImpl: MusiciansServiceImpl
-
-        @BeforeAll
-        fun setUp() {
-            val wireMockServer: WireMockServer
-        }
-
-    *//*    WireMockServer wireMockServer = new WireMockServer(wireMockConfig().port(8089)); //No-args constructor will start on port 8080, no HTTPS
-    wireMockServer.start();
-
-// Do some stuff
-
-    WireMock.reset();
-
-// Finish doing stuff
-
-    wireMockServer.stop();*//*
-
-
-
-    @Test
-    fun getAllMusicians() {
-    }
-
-    @Test
-    fun getMusicianByName() {
-    }
-
-    @Test
-    fun deleteAllMusicians() {
-    }
-
-    @Test
-    fun registerNewMusician() {
-    }
-
-    @Test
-    fun registerNewMusicians() {
-    }*/
 }
