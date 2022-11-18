4d133b4648a4a4fdf37f53528ab00c603222bdeb
anilogutveren <35260926+anilogutveren@users.noreply.github.com>
2022-11-18 23:55:05 +0100

    News can be fetched in v-cards

--- src/frontend/src/pages/HomePage.vue
+++ src/frontend/src/pages/HomePage.vue
@@ -1,16 +1,24 @@
 <template>
   <div class="container">
     <h2>Homepage</h2>
-    <img alt="Fts logo" src="../assets/Goodbye_Prophecy.jpeg">
+    <img alt="Goodbye_Prophecy logo" src="../assets/Goodbye_Prophecy.jpeg">
     <hr>
     <p>{{ admin }}</p>
     <br>
     <!--    <iframe width="40%" height="120" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/108672044&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/officialmetallica" title="Metallica" target="_blank" style="color: #cccccc; text-decoration: none;">Metallica</a> · <a href="https://soundcloud.com/officialmetallica/metallica-master-of-puppets" title="Master Of Puppets (From the Motion Picture Metallica Through the Never)" target="_blank" style="color: #cccccc; text-decoration: none;">Master Of Puppets (From the Motion Picture Metallica Through the Never)</a></div>-->
     <br>
+    <div class="d-flex flex-wrap flex-row justify-content-center align-items-center">
+      <div v-for="newsJson in newsList" class="card m-1">
+        <ul>
+          {{ newsJson.news }}
+        </ul>
+      </div>
+    </div>
   </div>
   <hr>
   <button class="button-container d-flex  flex-column align-items-center"
-          @click="getNews">Fetch News</button>
+          @click="getNews">Fetch News
+  </button>
   <hr>
 </template>
 
@@ -22,7 +30,8 @@ export default {
   name: "HomePage",
   data() {
     return {
-      admin: ""
+      admin: "",
+      newsList: []
     }
   },
   methods: {
@@ -30,33 +39,21 @@ export default {
       let newsLink = "http://localhost:8085/news/latest"
       customAxios.get(newsLink).then(response => {
             let responseData = response.data;
-            console.log(responseData)
+            console.log(responseData);
+            for (let key in responseData) {
+              this.newsList.push(responseData[key])
+            }
           }
-      ).catch(function (error) {
-        if (error.response) {
-          // The request was made and the server responded with a status code
-          // that falls out of the range of 2xx
-          console.log("RESPONSE ERROR:" +  error.response.data);
-          console.log("RESPONSE ERROR:" + error.response.status);
-          console.log("RESPONSE ERROR:" + error.response.headers);
-        } else if (error.request) {
-          // The request was made but no response was received
-          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-          // http.ClientRequest in node.js
-          console.log("REQUEST ERROR:" + error.request);
-        } else {
-          // Something happened in setting up the request that triggered an Error
-          console.log('Error', error.message);
-        }
-        console.log(error.config);
-      });
+      )
     }
   }
 }
 </script>
 
 <style>
-body {background-color: black;}
+body {
+  background-color: black;
+}
 
 .container {
   background-color: black;
